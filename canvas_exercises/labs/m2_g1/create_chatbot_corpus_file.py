85f6222620e4407e28421a9eab193bafb8b7609e
FenoCode <crumley.alex@hotmail.com>
2026-02-15 15:13:05 -0500

    feat: adds conversion of markdown files to plaintext formatting

--- canvas_exercises/labs/m2_g1/create_chatbot_corpus_file.py
+++ canvas_exercises/labs/m2_g1/create_chatbot_corpus_file.py
@@ -1,11 +1,29 @@
+import os
 from pathlib import Path
+import pypandoc
 
 # Retrieve corpus from '/dataset'
-doc_files = list(Path("F:\CS524\cs524-chatbot-project\dataset").rglob('*.txt'))
-doc_content_list = [file.read_text(encoding='utf-8', errors='replace') for file in doc_files]
+
+# Coalescing of .txt files
+txt_doc_files = list(Path("F:\CS524\cs524-chatbot-project\dataset").rglob('*.txt'))
+txt_doc_files = [f for f in txt_doc_files if os.path.basename(f) != "chatbot_corpus.txt"]
+doc_content_list = [file.read_text(encoding='utf-8', errors='replace') for file in txt_doc_files]
+
+# Conversion and coalescing of .md files
+md_doc_files = list(Path("F:\CS524\cs524-chatbot-project\dataset").rglob('*.md'))
+md_doc_files = [f for f in md_doc_files if os.path.basename(f) != "dataset.md"]
+md_content_list = [file.read_text(encoding='utf-8', errors='replace') for file in md_doc_files]
+
+# Conversion and coalescing of .csv files
+csv_doc_files = list(Path("F:\CS524\cs524-chatbot-project\dataset").rglob('*.csv'))
+print(csv_doc_files)
+
+
+# Converts markdown formatted docs to plain text
+md_content_list = [pypandoc.convert_text(file.read_text(encoding='utf-8', errors='replace'), 'plain', format='markdown') for file in md_doc_files]
 
 # Write corpus to file
-with open("chatbot_corpus.txt", "w", encoding='utf-8', errors='replace') as f:
+with open("F:\CS524\cs524-chatbot-project\dataset\chatbot_corpus.txt", "w", encoding='utf-8', errors='replace') as f:
     for content in doc_content_list:
         # Clean content allowing only valid UTF-8 characters
         content = ''.join(char for char in content if char.isprintable() or char.isspace())

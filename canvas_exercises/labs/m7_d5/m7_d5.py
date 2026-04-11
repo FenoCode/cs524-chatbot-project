e3dda505e97cd78709eef52ddbd466ef0c21e225
FenoCode <crumley.alex@hotmail.com>
2026-04-11 13:53:58 -0400

    feat: enhanced summarization text spllitting

--- canvas_exercises/labs/m7_d5/m7_d5.py
+++ canvas_exercises/labs/m7_d5/m7_d5.py
@@ -10,7 +10,7 @@ summary = []
 text = open("F:\CS524\cs524-chatbot-project-refresh\cs524-chatbot-project\canvas_exercises\labs\m7_d5\m7_d5.txt", 'r').read()
 
 # Tokenize and encode text and generate summary
-inputs = tokenizer.encode("summarize: " + text, return_tensors="pt")
+inputs = tokenizer.encode("summarize: " + text, max_length=1024, truncation=True, padding=True, return_overflowing_tokens=True, stride=128,return_tensors="pt")
 
 for input_slice in inputs.split(1024, dim=1):
     summary_ids = model.generate(input_slice, min_length=40, max_length=100, length_penalty=2.0, num_beams=4, early_stopping=True)
